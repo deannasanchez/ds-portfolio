28204621110a412933185018f02497e124accf62
Dee Lynn <deannalguzman@gmail.com>
2020-02-14 15:23:35 -0800

    fixing routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/NavBar/navbar";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -11,10 +11,12 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/work" component={Work} />
         <Route exact path="/contact" component={Contact} />
+      </Switch>
     </Router>
 
   );
